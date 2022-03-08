1d8cb61b571aeb0c7ca82acf81ce12cb14944642
LucassNogueira <simplylucasjason@gmail.com>
2022-03-08 11:54:55 -0600

    done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const foods = ["spaghetti", "ice cream", "sushi", "bologna", "cheese"];
+  const list = foods
+    .filter((element, index) => {
+      return element.includes(userInput);
+    })
+    .map((el, index) => {
+      return <h2 key={index}>{el}</h2>;
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(e) => setUserInput(e.target.value)} />
+      {list}
     </div>
   );
 }
